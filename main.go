07d844e251fdefc97acce34277b123a1572feed1
aww <emma@emaw.dev>
2024-01-14 17:39:38 -0500

    Add github.com/joho/godotenv package for environment variable management

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import (
 	"log"
 	"os"
 	"temotes/temotes/api"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	app := api.SetupServer()
 	log.Fatal(app.Listen(os.Getenv("SERVER_ADDR")))
 }
